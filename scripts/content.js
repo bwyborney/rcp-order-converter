a144b6ab6fe01f42406be59a4b3cfcf87043b6d0
Ben Wyborney <bwyborney@gmail.com>
2023-06-21 21:44:15 -0700

    Updated to work with new RCP site
    The extension now works on the new "invoice" page instead of the "order" page. Now injects a button into the page for ease-of-use. The new RCP site is much cleaner and doesn't contain false junk data, so this code is much shorter.

---
+++ scripts/content.js
@@ -0,0 +1,74 @@
+function orderConverter() {
+    // Initialize some arrays to hold all the important information
+    let skus = [];
+    let quantities = [];
+    let costs = [];
+
+    // Find the table containing the relevant data, which is the second invoice-table
+    const tables = document.getElementsByClassName('invoice-table')[2];
+    // Find the second child of the table, which is the tbody
+    const invoiceTable = tables.children[1];
+    
+    // Loop through each row and then each td in the tbody to pick out the relevant information
+    let counter = 0;
+    for (let row of invoiceTable.children) {
+        skus[counter] = row.children[0].textContent;
+        quantities[counter] = row.children[2].textContent;
+        costs[counter] = row.children[3].textContent;
+        counter += 1;
+    }
+
+    // Get the order number
+    let orderNumber = document.getElementsByClassName('invoice-table')[0]
+        .children[1]
+        .children[0]
+        .children[1]
+        .textContent;
+
+    // Create a list that contains all the data we just pulled
+    let csvData = [];
+    for (let sku in skus) {
+        csvData[sku] =  [skus[sku], skus[sku], quantities[sku], costs[sku], ' ', ' ', ' ', ' ', orderNumber, ' ', ' '];
+        // Those blank spaces are going to fill in the columns like 'Supplier' and 'Use fifo,' since they're not needed
+    }
+
+    // Create the header row for the CSV file
+    let csv = 'Catalog Item,Supplier SKU,Qty,Cost,Price,Note,Location,Supplier,Vendor Order ID,Backordered tickets,Use fifo\n';
+    // Add the data from the csvData variable, one index at a time. Each index will be a row in the CSV file
+    csvData.forEach(row => {
+        csv += row.join(',');
+        csv += '\n';
+    });
+    // Generate and download the CSV file
+    let fileName = `${orderNumber}.csv`;
+    let hiddenElement = document.createElement('a');
+    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+    hiddenElement.target = '_blank';
+    hiddenElement.download = fileName;
+    hiddenElement.click();
+    
+}
+
+// Add a button to download the CSV file
+function addButton() {
+    // Find the header bar which contains the other download buttons
+    const headerBar = document.getElementsByClassName('invoice-detail-header-bar')[0];
+    const injectPoint = headerBar.children[1];
+
+    // Create the download button element
+    const downloadButton = document.createElement('div');
+    downloadButton.innerHTML = "<a href = '#' class = 'print_pdf'><span><i style='font-style: normal; font-size: 1.5rem'>&#128202;</i> Download CSV</span></a>";
+    downloadButton.onclick = () => orderConverter(); 
+    downloadButton.style.marginRight = '1rem';
+    downloadButton.style.marginLeft = '1rem';
+
+    // Insert the download button
+    injectPoint.insertAdjacentElement('afterend', downloadButton);
+}
+
+// Wait for the page to finish loading before adding the download button
+setTimeout(() => { 
+    addButton();
+}, 2000);
+
+
