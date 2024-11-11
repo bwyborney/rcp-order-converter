e27e198c94bfdda6466f04308a88a7340891d45c
Ben Wyborney <bwyborney@gmail.com>
2024-11-11 07:40:35 -0800

    continued on cellhelmet
    Got the content script working - had to use a mutation observer since the page takes anywhere from two to ten seconds to load. Now just need to write CSV generator

--- scripts/cellhelmet.js
+++ scripts/cellhelmet.js
@@ -1,19 +1,30 @@
-
-
+// Generate and download the CSV file
+function generate() {
+    let skus = [];
+    let quantities = [];
+    let costs = [];
+}
+// Add the "download CSV" button
 function addButton() {
     let targetSibling = document.getElementsByClassName('order-history-summary-button-reorder')[0];
     let button = document.createElement('a');
     button.classList = 'orderConverterButton';
     button.innerText = 'Download CSV';
-
+    button.addEventListener('click', () => {generate()});
     targetSibling.insertAdjacentElement('afterend', button);
-    console.log('yuh');
 }
 
+// Check and see if the button elements are present yet
+function checkForContents() {
+    let buttonList = document.getElementsByClassName('order-history-summary-button-reorder');
+    if (buttonList.length > 0) {
+        addButton();
+    }
+}
 
-// Wait for the page to finish loading before adding the download button
-setTimeout(() => { 
-    addButton();
-}, 1000);
+// Begin a mutation observer to listen for the contents of the page to load
+const config = {childList: true, attributes: true};
+let watch = document.getElementById('main');
+const observer = new MutationObserver(checkForContents);
+observer.observe(watch, config);
 
-console.log('yuh');
