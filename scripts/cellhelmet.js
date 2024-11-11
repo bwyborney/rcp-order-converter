7cc5b5bdaa070906a04b91e279e906f09794daf5
Ben Wyborney <bwyborney@gmail.com>
2024-11-11 10:30:41 -0800

    continued
    struggling with a onedrive glitch

--- scripts/cellhelmet.js
+++ scripts/cellhelmet.js
@@ -3,7 +3,40 @@ function generate() {
     let skus = [];
     let quantities = [];
     let costs = [];
+    let orderNumber;
+
+    let skuDivs = document.getElementsByClassName('product-line-sku-value');
+    for (let sd = 0; sd < skuDivs.length; sd++) {
+        skus[sd] = skuDivs[sd].innerText;
+    }
+
+    let qtyDivs = document.getElementsByClassName('transaction-line-views-quantity-amount-value');
+    for (let qt = 0; qt < qtyDivs.length; qt++) {
+        quantities[qt] = qtyDivs[qt].innerText;
+    }
+
+    let costDivs = document.getElementsByClassName('transaction-line-views-quantity-amount-item-amount');
+    for (let co = 0; co < costDivs.length; co++) {
+        costs[co] = costDivs[co].innerText;
+    }
+
+    let csvData = [];
+    for (let sku in skus) {
+        csvData[sku] =  [skus[sku], skus[sku], quantities[sku], costs[sku], ' ', ' ', ' ', ' ', orderNumber, ' ', ' '];
+    }
+    let csv = 'Catalog Item,Supplier SKU,Qty,Cost,Price,Note,Location,Supplier,Vendor Order ID,Backordered tickets,Use fifo\n';
+    csvData.forEach(row => {
+        csv += row.join(',');
+        csv += '\n';
+    });
+    let fileName = `${orderNumber}.csv`;
+    let hiddenElement = document.createElement('a');
+    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+    hiddenElement.target = '_blank';
+    hiddenElement.download = fileName;
+    hiddenElement.click();
 }
+
 // Add the "download CSV" button
 function addButton() {
     let targetSibling = document.getElementsByClassName('order-history-summary-button-reorder')[0];
