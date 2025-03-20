66832448086285bf864e8a498428aee529835c95
Ben Wyborney <bwyborney@gmail.com>
2025-03-20 07:49:10 -0700

    fixed and tested the Hyla button

--- scripts/hyla.js
+++ scripts/hyla.js
@@ -1,5 +1,5 @@
 // Handle the conversion
-convertAndCopy() {
+function convertAndCopy() {
     console.log('nice');
 }
 
@@ -7,44 +7,36 @@ convertAndCopy() {
 function addButton() {
     // Identify the navbar with download buttons using its unique class
     const orderRow = document.getElementsByClassName('order-table')[0];
-    // Identify the container div with the actual buttons
-    let buttonRow;
-    for (let c = 0; c < orderRow.children.length; c++) {
-        if (orderRow.children[c].classList.includes('col-xs-6')) {
-            buttonRow = orderRow.children[c];
+    if (orderRow && orderRow.children) {
+        // Identify the container div with the actual buttons
+        let buttonRow;
+        for (let c = 0; c < orderRow.children.length; c++) {
+            if (orderRow.children[c].classList.contains('col-xs-6')) {
+                buttonRow = orderRow.children[c];
+            }
         }
-    }
-    // That container div typically contains one or more spacers divs.
-    // Find the last one
-    let buttonPlaceholder;
-    for (let b = 0; b < buttonRow.children.length; b++) {
-        if (buttonRow.children[b].classList.includes('clearfix')) {
-            buttonPlaceholder = buttonRow.children[b];
+        // That container div typically contains one or more spacers divs.
+        // Find the last one
+        let buttonPlaceholder;
+        for (let b = 0; b < buttonRow.children.length; b++) {
+            if (buttonRow.children[b].classList.contains('clearfix')) {
+                buttonPlaceholder = buttonRow.children[b];
+            }
         }
+        // Now generate the button        
+        let button = document.createElement('a');
+        button.href = '#';
+        button.classList = 'text-primary pull-right download-button material-icons-container';
+        let buttonIcon = document.createElement('span');
+        buttonIcon.classList = 'material-icons hidden-xs';
+        button.innerHTML = `<span class="material-icons hidden-xs">content_copy</span> Copy to Convert`;
+        button.appendChild(buttonIcon);
+        button.addEventListener('click', () => {convertAndCopy()});
+        // Add the button and remove that hidden placeholder div
+        buttonPlaceholder.parentElement.appendChild(button);
+        buttonPlaceholder.remove();
     }
-    // Now generate our button. Here's what it needs to match:
-    /*
-    <a 
-        href="#" 
-        class="text-primary pull-right download-button material-icons-container "
-    >
-        <span 
-            class="material-icons hidden-xs"
-        >
-            get_app
-        </span> 
-        IMEI List
-    </a>
-    */
-   let button = document.createElement('a');
-   button.href = '#';
-   button.classList = 'text-primary pull-right download-button material-icons-container';
-   let buttonIcon = document.createElement('span');
-    buttonIcon.classList = 'material-icons hidden-xs';
-    buttonIcon.innerText = 'content_copy';
-    button.appendChild(buttonIcon);
-    button.innerText = 'Copy to Convert';
-    button.addEventListener('click', () => {convertAndCopy()});
+    
 
 }
 
